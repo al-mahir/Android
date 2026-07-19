4b8f7aa76598be2d255517b39031aa1c333e6bac
abdelrahman <abdelrahmankarim7@gmail.com>
2026-07-19 04:05:56 +0300

    Extend Mushaf presentation MVI state with new UI interaction and recording intents

--- mushaf/presentation/src/main/java/com/example/mushaf/presentation/state/MushafIntent.kt
+++ mushaf/presentation/src/main/java/com/example/mushaf/presentation/state/MushafIntent.kt
@@ -1,5 +1,7 @@
 package com.example.mushaf.presentation.state
 
+import com.example.mushaf.domain.model.MushafMode
+
 sealed interface MushafIntent {
     data class LoadPage(val page: Int) : MushafIntent
     data class ToggleTajweed(val enabled: Boolean) : MushafIntent
@@ -7,4 +9,10 @@ sealed interface MushafIntent {
     data object StartFollowAlongPreview : MushafIntent
     data object StopFollowAlongPreview : MushafIntent
     data object Retry : MushafIntent
+    data object ToggleBars : MushafIntent
+    data class SetMode(val mode: MushafMode) : MushafIntent
+    data object ToggleAyahVisibility : MushafIntent
+    data object RevealNextWord : MushafIntent
+    data object RevealNextAyah : MushafIntent
+    data object ToggleRecording : MushafIntent
 }
