75a54bcc1b4ce18e403fc583c553be6b6bcb19bf
yasse <yassenyasooo50@gmail.com>
2026-07-31 22:37:24 +0300

    update: sheikh app and presentation layer to support offline state rendering

--- sheikh/presentation/src/main/java/com/iti/sheikh/presentation/home/state/SheikhHomeUiState.kt
+++ sheikh/presentation/src/main/java/com/iti/sheikh/presentation/home/state/SheikhHomeUiState.kt
@@ -4,9 +4,10 @@ import androidx.annotation.StringRes
 
 data class SheikhHomeUiState(
     val isLoading: Boolean = true,
-    @StringRes val errorMessageRes: Int? = null,
+    val errorMessageRes: Int? = null,
     val initials: String? = null,
     val avatarUrl: String? = null,
+    val isOffline: Boolean = false,
 ) {
     val hasError: Boolean get() = errorMessageRes != null
 }
