ded3b0d816c3e7a602457aac49e9ba055be681d1
yasse <yassenyasooo50@gmail.com>
2026-08-09 23:17:14 +0300

    configure application entry points and navigation for updated network modules

--- app/src/main/java/com/iti/al_mahir/AlMahirApp.kt
+++ app/src/main/java/com/iti/al_mahir/AlMahirApp.kt
@@ -7,7 +7,6 @@ import com.iti.data.core.token.TokenStore
 import com.iti.data.di.almahirDataModule
 import com.iti.data.payment.di.paymentDataModule
 import com.iti.data.user.auth.di.authDataModule
-import com.iti.domain.auth.MeetingAuthTokenProvider
 import com.iti.domain.auth.MeetingCurrentUserProvider
 import com.iti.domain.auth.di.authDomainModule
 import com.iti.domain.payment.di.paymentDomainModule
@@ -49,18 +48,6 @@ class AlMahirApp : Application() {
                             agoraAppId = BuildConfig.AGORA_APP_ID,
                             enableHttpLogging = BuildConfig.DEBUG
                         ) 
-                    }
-                    single<MeetingAuthTokenProvider> {
-                        val tokenStore = GlobalContext.get().get<TokenStore>()
-                        val refresher = com.iti.data.core.token.TokenRefresher(
-                            tokenStore = tokenStore,
-                            refreshEndpoint = com.iti.data.core.network.AlmahirApi.Auth.REFRESH,
-                        )
-                        object : MeetingAuthTokenProvider {
-                            override suspend fun currentToken(): String? = tokenStore.getTokens()?.accessToken
-                            override suspend fun refreshToken(): String? = refresher.refresh()?.accessToken
-                            override suspend fun onAuthenticationExpired() = tokenStore.clear()
-                        }
                     }
                     single<MeetingCurrentUserProvider> {
                         MeetingCurrentUserProvider {
