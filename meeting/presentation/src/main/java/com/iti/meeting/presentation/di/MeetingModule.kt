be6e0eaeadee0e96b3ac95de45efa263840acd28
yasse <yassenyasooo50@gmail.com>
2026-08-18 01:24:37 +0300

    update meeting resources and module configuration

--- meeting/presentation/src/main/java/com/iti/meeting/presentation/di/MeetingModule.kt
+++ meeting/presentation/src/main/java/com/iti/meeting/presentation/di/MeetingModule.kt
@@ -1,6 +1,7 @@
 package com.iti.meeting.presentation.di
 
 import com.iti.meeting.presentation.call.CallViewModel
+import com.iti.meeting.presentation.call.audio.AudioRouteController
 import com.iti.meeting.presentation.call.session.CallSessionController
 import com.iti.meeting.domain.config.MeetingKitConfig
 import com.iti.meeting.domain.repository.MeetingRepository
@@ -9,13 +10,14 @@ import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
 
 val meetingCallModule = module {
+    single { AudioRouteController(context = androidContext()) }
     single {
         CallSessionController(
             appContext = androidContext(),
             config = get<MeetingKitConfig>(),
             repository = get<MeetingRepository>(),
+            audioRoutes = get<AudioRouteController>(),
         )
     }
     viewModel { CallViewModel(controller = get<CallSessionController>()) }
 }
-
